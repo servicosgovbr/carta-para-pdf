5527a52a1d624b9cde43b1f991bccfcd0e125f3d
Glauber Ramos <glauberamos@gmail.com>
2015-10-05 17:45:16 -0300

    Buscando todos serviços de um orgão baseado na sua url

--- script/script.js
+++ script/script.js
@@ -1,73 +1,92 @@
 var xmlDoc;
-var servicos = [
-	'banco-internacional-de-objetos-educacionais.xml',
-	'ciencia-sem-fronteiras.xml',
-	'dominio-publico-biblioteca-digital.xml',
-	'enem-exame-nacional-do-ensino-medio.xml',
-	'portal-do-professor.xml',
-	'programa-de-financiamento-estudantil-fies.xml',
-	'pronatec-programa-nacional-de-acesso-ao-ensino-profissional-e-emprego.xml',
-	'prouni-programa-universidade-para-todos.xml',
-	'sisu-sistema-de-selecao-unificada.xml',
-	'sisutec-sistema-de-selecao-unificada-da-educacao-profissional-e-tecnologica.xml',
-	'tv-escola.xml'
-]
+var servicos = {};
+
+var docDefinition = {
+	content: [],
+	styles: {
+	title: {
+       fontSize: 30,
+       bold: true,
+       color: '#2c66ce'
+     },
+     header: {
+       fontSize: 22,
+       bold: true
+     },
+     subheader: {
+       fontSize: 18,
+       bold: true
+     },
+     list: {
+     	fontSize: 14,
+     	blod: true
+     },
+     paragraph: {
+     	fontSize: 16
+     }
+   }
+};
+
+var initialDocDefinition = function() {	
+	docDefinition.content.push('\n');
+	docDefinition.content.push('\n');
+	docDefinition.content.push({ text: 'Carta do serviço', style: 'title', alignment: 'center'});
+	docDefinition.content.push('\n');
+	docDefinition.content.push('\n');
+	docDefinition.content.push({ text: 'Ministério da educação MEC', style: 'header', pageBreak: 'after', alignment: 'center'});
+}
 
 $(function() {
  	$('#gerar-pdf').on('click', function() {
- 		var docDefinition = {
-			content: [],
-			styles: {
-			title: {
-		       fontSize: 30,
-		       bold: true,
-		       color: '#2c66ce'
-		     },
-		     header: {
-		       fontSize: 22,
-		       bold: true
-		     },
-		     subheader: {
-		       fontSize: 18,
-		       bold: true
-		     },
-		     list: {
-		     	fontSize: 14,
-		     	blod: true
-		     },
-		     paragraph: {
-		     	fontSize: 16
-		     }
-		   }
-		};
+ 		servicos = {};
+		initialDocDefinition();
+
+		var urlServico = $('#url-servico').val();
 		
-		var promises = [];
+		$.ajax({ 
+			method: 'GET',
+		  url: 'https://api.github.com/repos/servicosgovbr/cartas-de-servico/contents/cartas-servico/v3/servicos'
+		}).then(function(data) {
+			var promises = [];
 
-		docDefinition.content.push('\n');
-		docDefinition.content.push('\n');
-		docDefinition.content.push({ text: 'Carta do serviço', style: 'title', alignment: 'center'});
-		docDefinition.content.push('\n');
-		docDefinition.content.push('\n');
-		docDefinition.content.push({ text: 'Ministério da educação MEC', style: 'header', pageBreak: 'after', alignment: 'center'});
+			$(data).each(function(index, file) {
+				var promise = $.ajax({ 
+					method: 'GET',
+				 	url:"https://raw.githubusercontent.com/servicosgovbr/cartas-de-servico/master/cartas-servico/v3/servicos/" + file.name
+				});
 
- 		for (i = 0; i < servicos.length; i++) {
-			var promise = $.ajax({ 
-				method: 'GET',
-			 	url:"https://raw.githubusercontent.com/servicosgovbr/cartas-de-servico/master/cartas-servico/v3/servicos/" + servicos[i]
+				promise.then(function(data) {
+		    		var parser = new DOMParser();
+		    		xmlDoc = parser.parseFromString(data, "text/xml");
+
+		    		var orgaoId;
+
+		    		if (xmlDoc && xmlDoc.getElementsByTagName('orgao')[0]) {
+		    			orgaoId = xmlDoc.getElementsByTagName('orgao')[0].getAttribute('id');
+		    		} else {
+		    			orgaoId = '';
+		    		}
+
+		    		if (servicos[orgaoId]) {
+		    			servicos[orgaoId].push(data);
+		    		} else {
+		    			servicos[orgaoId] = [data];
+		    		};
+				});
+
+				promises.push(promise);
 			});
 
-			promise.then(function(data) {
-	    		var servicoParser = new ServicoParser();
-	    		var servicoDocument = servicoParser.parseXml(data);
+			Q.all(promises).then(function() {
+				$(servicos[urlServico]).each(function(index, xmlServico) {
+					var servicoParser = new ServicoParser();
+					var servicoDocument = servicoParser.parseXml(xmlServico);
 
-	    		docDefinition.content = docDefinition.content.concat(servicoDocument);
+					docDefinition.content = docDefinition.content.concat(servicoDocument);
+				});
+				
+				pdfMake.createPdf(docDefinition).open();
 			});
-
-			promises.push(promise);
-		}
-
-		Q.all(promises).then(function() {
-			pdfMake.createPdf(docDefinition).open();
 		});
 	});
 });
