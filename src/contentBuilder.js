be906aafef7ff518253d103eb52e284879090dff
Glauber Ramos <glauberamos@gmail.com>
2015-10-08 18:44:31 -0300

    Removendo código não usado

--- src/contentBuilder.js
+++ src/contentBuilder.js
@@ -32,28 +32,6 @@ var ContentBuilder = function(servicoObject) {
 		});
 	}
 
-	// function addValuesIntoDocument() {
-	// 	docContent.push({ text: 'Etapas para a realização deste serviço', style: 'subheader' });
-	// 	addNewLine();
-	// 	docContent.push({ text: 'Etapa 1', style: 'subheader' });
-	// 	addNewLine();
-	// 	docContent.push({ text: 'Canais de Prestação', style: 'subheader'});
-	// 	addNewLine();
-	// 	docContent.push({ ul: canaisDePrestacao, style: 'list' });
-	// 	addNewLine();
-	// 	docContent.push({ text: 'Segmentos da Sociedade', style: 'subheader'});
-	// 	addNewLine();
-	// 	docContent.push({ ul: [ segmentosDaSociedade ], style: 'list' });
-	// 	addNewLine();
-	// 	docContent.push({ text: 'Áreas de Interesse', style: 'subheader'});
-	// 	addNewLine();
-	// 	docContent.push({ ul: areasDeInteresse, style: 'list' });
-	// 	addNewLine();
-	// 	docContent.push({ text: 'Orgão responsável', style: 'subheader'});
-	// 	addNewLine();
-	// 	docContent.push({ ul: [ orgao ], style: 'paragraph', pageBreak: 'after' });
-	// }
-
 	function buildContent() {
 		buildCapaServico();
 		buildPrimeiraPagina();
