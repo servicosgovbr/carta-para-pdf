247effd587967c97ef075363f7137400a0351249
Glauber Ramos <glauberamos@gmail.com>
2015-10-08 15:56:26 -0300

    Adicionando servico writer

--- src/servicoParser.js
+++ src/servicoParser.js
@@ -1,6 +1,5 @@
 var ServicoParser = function() {
-	var segmentosDaSociedade, areasDeInteresse, canaisDePrestacao, orgao, xmlDoc;
-	var docContent = [];
+	var xmlDoc;
 	var returnObject = {};
 
 	function parseNome(xmlDoc) {
@@ -82,6 +81,7 @@ var ServicoParser = function() {
 
 		return values;
 	}
+	
 	function parseEtapas(xmlDoc) {
 		var etapas = [];
 
@@ -168,36 +168,6 @@ var ServicoParser = function() {
 
 	function parseOrgao(xmlDoc) {
 		return { id: $(xmlDoc).find("orgao").attr('id'), contato: $(xmlDoc).find("orgao contato").html() };
-	}
-
-	function addNewLine() {
-		docContent.push('\n');
-	}
-
-	function addValuesIntoDocument() {
-		docContent.push({ text: nome, style: 'header'});
-		addNewLine();
-		docContent.push({ text: descricao, style: 'paragraph' });
-		addNewLine();
-		docContent.push({ text: 'Etapas para a realização deste serviço', style: 'subheader' });
-		addNewLine();
-		docContent.push({ text: 'Etapa 1', style: 'subheader' });
-		addNewLine();
-		docContent.push({ text: 'Canais de Prestação', style: 'subheader'});
-		addNewLine();
-		docContent.push({ ul: canaisDePrestacao, style: 'list' });
-		addNewLine();
-		docContent.push({ text: 'Segmentos da Sociedade', style: 'subheader'});
-		addNewLine();
-		docContent.push({ ul: [ segmentosDaSociedade ], style: 'list' });
-		addNewLine();
-		docContent.push({ text: 'Áreas de Interesse', style: 'subheader'});
-		addNewLine();
-		docContent.push({ ul: areasDeInteresse, style: 'list' });
-		addNewLine();
-		docContent.push({ text: 'Orgão responsável', style: 'subheader'});
-		addNewLine();
-		docContent.push({ ul: [ orgao ], style: 'paragraph', pageBreak: 'after' });
 	}
 
 	function parseXml(data) {
